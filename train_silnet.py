7160ce776d62884781026fd24c193cddfc08518a
breadbread1984 <breadbread1984@163.com>
2020-02-04 19:39:23 +0800

    update train

--- train_silnet.py
+++ train_silnet.py
@@ -2,7 +2,7 @@
 
 import os;
 import tensorflow as tf;
-from create_dataset import parse_function;
+from create_dataset import segment_parse_function;
 from models import SilNet;
 
 batch_size = 8;
@@ -16,7 +16,7 @@ def main():
     return tf.keras.losses.SparseCategoricalCrossentropy(from_logits = True)(labels, outputs);
   silnet.compile(optimizer = tf.keras.optimizers.Adam(0.001), loss = loss);
   # load dataset
-  trainset = 
+  trainset = tf.data.TFRecordDataset(os.path.join('datasets','synthesis.tfrecord')).repeat(-1).map(segment_parse_function).shuffle(batch_size).batch(batch_size).prefetch(tf.data.experimental.AUTOTUNE);
   # train
   silnet.fit(trainset, epochs = 20);
   if not os.path.exists('models'): os.mkdir('models');
